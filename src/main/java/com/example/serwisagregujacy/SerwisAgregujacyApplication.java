ae2954abdf95b4f872e974cd5a8c01e273dd1fc5
Monia <monika.misiurek01@gmail.com>
2022-06-11 11:06:51 +0200

    Initial commit

---
+++ src/main/java/com/example/serwisagregujacy/SerwisAgregujacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.serwisagregujacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisAgregujacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerwisAgregujacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serwisagregujacy/SerwisAgregujacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serwisagregujacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisAgregujacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
