906d81d6599f37910f60c07fc938746ce8591f99
Monia <monika.misiurek01@gmail.com>
2022-06-12 12:15:33 +0200

    feature/EA-9-Create_class_Events
    -created model class: Events

---
+++ src/main/java/com/example/serwisagregujacy/model/Contact.java
@@ -0,0 +1,32 @@
+package com.example.serwisagregujacy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "PHONE")
+    private long phone;
+    //TODO validation email
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "ADDRESS")
+    private String address;
+
+
+
+
+}
