2b1819c5b23b461d1a6e85dcff7874e58bca8576
Monia <monika.misiurek01@gmail.com>
2022-06-25 11:38:06 +0200

    EA-25- ContactController
    Create ContactController and ContactService

--- src/main/java/com/example/serwisagregujacy/controller/ContactController.java
+++ src/main/java/com/example/serwisagregujacy/controller/ContactController.java
@@ -1,7 +1,44 @@
 package com.example.serwisagregujacy.controller;
 
+import com.example.serwisagregujacy.model.Contact;
+import com.example.serwisagregujacy.serwice.ContactService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ContactController {
+
+    public final ContactService contactService;
+
+
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping("/contact")
+    public String getContact() {
+        return "contact/contact";
+    }
+
+    @PostMapping("/addNewContact")
+    public RedirectView postAddNewContact(Contact contact) {
+        contactService.addNewContact(contact);
+        return new RedirectView("/contact");
+    }
+
+    @PostMapping("/editContact/{id}")
+    public RedirectView postEditContact(Contact contact, @PathVariable Long id) {
+        contactService.editContact(contact);
+        return new RedirectView("/contact");
+    }
+
+    @PostMapping("delete/deleteContact/{id}")
+    public RedirectView postDeleteContact (Contact contact, @PathVariable Long id){
+        contactService.deleteContact(contact);
+        return new RedirectView("/contact");
+    }
+
 }
