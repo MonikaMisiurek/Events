bd6a02a6172d6dd4ad62e45d31a7d18a55d783a1
Marta <piszdomarty@gmail.com>
2022-06-25 11:41:10 +0200

    EA-22: UserRegistrationController & UserController
    zrobienie klas controllerów i servisowych

--- src/main/java/com/example/serwisagregujacy/serwice/UserRegistrationService.java
+++ src/main/java/com/example/serwisagregujacy/serwice/UserRegistrationService.java
@@ -1,7 +1,19 @@
 package com.example.serwisagregujacy.serwice;
 
+import com.example.serwisagregujacy.model.User;
+import com.example.serwisagregujacy.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRegistrationService {
+
+    private final UserRepository userRepository;
+
+    public UserRegistrationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void addUser(User user){
+        userRepository.save(user);
+    }
 }
