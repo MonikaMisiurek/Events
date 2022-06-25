03baf26380146a4142959e187b556171fc0ef23c
Monia <monika.misiurek01@gmail.com>
2022-06-25 15:29:52 +0200

    EA-29:Account

--- src/main/java/com/example/serwisagregujacy/controller/MyAccountController.java
+++ src/main/java/com/example/serwisagregujacy/controller/MyAccountController.java
@@ -1,9 +1,38 @@
 package com.example.serwisagregujacy.controller;
 
+import com.example.serwisagregujacy.model.Account;
+import com.example.serwisagregujacy.serwice.MyAccountService;
+import com.example.serwisagregujacy.serwice.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
 
 @Controller
 public class MyAccountController {
-    public static class EventsController {
+
+    private final MyAccountService myAccountService;
+    private final UserService userService;
+
+    public MyAccountController(MyAccountService myAccountService, UserService userService) {
+        this.myAccountService = myAccountService;
+        this.userService = userService;
     }
+
+
+
+    @GetMapping("/myAccount")
+    public String getMyAccount() {
+        return ("/myAccount");
+    }
+
+    @PostMapping("/editMyAccount")
+    public RedirectView postEditMyAccount(@Valid Account account) {
+        myAccountService.editMyAccount(account);
+        return new RedirectView("/myAccount");
+    }
+
+
 }
--- src/main/java/com/example/serwisagregujacy/serwice/MyAccountService.java
+++ src/main/java/com/example/serwisagregujacy/serwice/MyAccountService.java
@@ -1,7 +1,28 @@
 package com.example.serwisagregujacy.serwice;
 
+import com.example.serwisagregujacy.model.Account;
+import com.example.serwisagregujacy.model.Contact;
+import com.example.serwisagregujacy.repository.MyAccountRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyAccountService {
+    private final MyAccountRepository myAccountRepository;
+
+
+    public MyAccountService(MyAccountRepository myAccountRepository) {
+        this.myAccountRepository = myAccountRepository;
+    }
+
+
+    public void editMyAccount(Account account){
+        myAccountRepository.save(account);
+    }
+
+    // wyświetlanie eventów na które jest się zapisanym
+
+    // wypisywanie się z events
+
+
+
 }
