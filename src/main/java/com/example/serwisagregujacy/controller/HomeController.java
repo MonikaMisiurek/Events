19fbcee0e994bd333c0ad6658d69c9eb22d170e4
Monia <monika.misiurek01@gmail.com>
2022-06-25 12:09:04 +0200

    EA-14:Home Controller
    Create Home Controller

--- src/main/java/com/example/serwisagregujacy/controller/HomeController.java
+++ src/main/java/com/example/serwisagregujacy/controller/HomeController.java
@@ -1,7 +1,16 @@
 package com.example.serwisagregujacy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+
+    @GetMapping("/")
+    public String getHome() {
+        return "index";
+    }
+
+
 }
