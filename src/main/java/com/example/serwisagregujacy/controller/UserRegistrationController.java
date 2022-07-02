19d9f289608fe993dbf1f724825099832a3f0019
Marta <piszdomarty@gmail.com>
2022-07-02 10:51:01 +0200

    usuwanie eventRegistration
    klasy wykomentowane, przed usunięciem

--- src/main/java/com/example/serwisagregujacy/controller/UserRegistrationController.java
+++ src/main/java/com/example/serwisagregujacy/controller/UserRegistrationController.java
@@ -1,31 +1,31 @@
-package com.example.serwisagregujacy.controller;
+//package com.example.serwisagregujacy.controller;
+//
+//import com.example.serwisagregujacy.dto.UserDTO;
+//import com.example.serwisagregujacy.service.UserRegistrationService;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.view.RedirectView;
+//
+//@Controller
+//public class UserRegistrationController {
+//
+//    private final UserRegistrationService userRegistrationService;
+//    public UserRegistrationController(UserRegistrationService userRegistrationService) {
+//        this.userRegistrationService = userRegistrationService;
+//    }
 
-import com.example.serwisagregujacy.dto.UserDTO;
-import com.example.serwisagregujacy.service.UserRegistrationService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
-
-@Controller
-public class UserRegistrationController {
-
-    private final UserRegistrationService userRegistrationService;
-    public UserRegistrationController(UserRegistrationService userRegistrationService) {
-        this.userRegistrationService = userRegistrationService;
-    }
-   @GetMapping("/registration")
-   public String getRegistration(){
-        return ("/registration");
-   }
-
-    @PostMapping("/registration")
-    public RedirectView postAddPerson(@PathVariable UserDTO user) {
-        userRegistrationService.addUser(user);
-        return new RedirectView("/registration");
-    }
+//    @GetMapping("/registration")
+//    public String getRegistration(){
+//        return ("/registration");
+//    }
+//    @PostMapping("/registration")
+//    public RedirectView postAddPerson(@PathVariable UserDTO user) {
+//        userRegistrationService.addUser(user);
+//        return new RedirectView("/registration");
+//    }
 
 
 }
